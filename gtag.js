52dac487c61bc2aad9f32c05cd2d7dd35d1f9bd6
jukyellow <45334819+jukyellow@users.noreply.github.com>
2021-02-26 18:07:38 +0900

    Site updated: 2021-02-26 18:07:37

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EG1ZFE1SGM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EG1ZFE1SGM');
+</script>
